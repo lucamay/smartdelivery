f5179e589d840ff33b3a51d48f1698b4ce671484
Lupe Canaviri <lupemaydana@gmail.com>
2017-10-04 13:07:32 -0400

    añadiendo nuevo login

---
+++ Frontend/src/app/registrate/colaborador-registrar.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+@Component({
+	selector: 'colaborador-adicionar',
+	templateUrl: './colaborador-adicionar.html'
+})
+export class ColaboradorAdicionarComponent {
+  title = 'Adicionar colaborador';
+}
